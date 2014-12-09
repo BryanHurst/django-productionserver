4b792877ef50d3027f1726efce6b1a20eff13078
Bryan <bryan@newlinetechnicalinnovations.com>
2014-12-09 15:44:47 -0700

    Basic new version with limited options for initial testing. NOTE: nginx has not been configured yet, so this won't serve static files technically

--- productionserver/management/commands/runproductionserver.py
+++ productionserver/management/commands/runproductionserver.py
@@ -2,8 +2,10 @@ import sys
 import os
 
 from django.core.management.base import BaseCommand
-
+from django.core.servers.basehttp import get_internal_wsgi_application
 from optparse import make_option
+
+import cherrypy
 
 
 class Command(BaseCommand):
@@ -14,6 +16,8 @@ class Command(BaseCommand):
     Futures (https://pypi.python.org/pypi/futures) is required.
     """
     args = "[--option=value, use `runproductionserver help` for help]"
+
+    options = None
 
     option_list = BaseCommand.option_list + (
         make_option('--host',
@@ -31,4 +35,31 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
-        pass
+        self.options = options
+
+        # Get this Django Project's WSGI Application and mount it
+        application = get_internal_wsgi_application()
+        cherrypy.tree.graft(application, "/")
+
+        # Unsubscribe the default server
+        cherrypy.server.unsubscribe()
+
+        # Instantiate a new server
+        server = cherrypy._cpserver.Server()
+
+        # Configure the server
+        server.socket_host = self.options['host']
+        server.socket_port = self.options['port']
+        server.thread_pool = 30
+
+        # For SSL Support
+        # server.ssl_module = 'pyopenssl'
+        # server.ssl_certificate = 'ssl/certificate.crt'
+        # server.ssl_private_key = 'ssl/private.key'
+        # server.ssl_certificate_chain = 'ssl/bundle.crt
+
+        # Subscribe the new server
+        server.subscribe()
+
+        cherrypy.engine.start()
+        cherrypy.engine.block()
