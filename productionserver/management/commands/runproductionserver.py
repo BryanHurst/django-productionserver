39c7e03077406023e08e37db996b33d3d456e5a2
Bryan <bryan@newlinetechnicalinnovations.com>
2014-12-10 11:09:56 -0700

    Basic writing of nginx conf file. Needs to be cleaned up.

--- productionserver/management/commands/runproductionserver.py
+++ productionserver/management/commands/runproductionserver.py
@@ -1,9 +1,12 @@
 import sys
 import os
+import shutil
 
 from django.core.management.base import BaseCommand
 from django.core.servers.basehttp import get_internal_wsgi_application
+from django.conf import settings
 from optparse import make_option
+from tempfile import mkstemp
 
 import cherrypy
 
@@ -18,6 +21,7 @@ class Command(BaseCommand):
     args = "[--option=value, use `runproductionserver help` for help]"
 
     options = None
+    PRODUCTIONSERVER_DIR = os.path.dirname(os.path.abspath(__file__))
 
     option_list = BaseCommand.option_list + (
         make_option('--host',
@@ -49,7 +53,7 @@ class Command(BaseCommand):
 
         # Configure the server
         server.socket_host = self.options['host']
-        server.socket_port = self.options['port']
+        server.socket_port = self.options['port'] + 1
         server.thread_pool = 30
 
         # For SSL Support
@@ -61,5 +65,39 @@ class Command(BaseCommand):
         # Subscribe the new server
         server.subscribe()
 
+        # Get the DJANGO_BASE nginx conf file and copy it to the running project's base dir as nginx.conf.
+        # Startup nginx with a passed in conf file of the one in the project's base dir.
+        # nginx -c settings.BASE_DIR/nginx.conf
+        if os.path.isfile(os.path.join(settings.BASE_DIR, 'nginx.conf')):
+            os.remove(os.path.join(settings.BASE_DIR, 'nginx.conf'))
+
+        shutil.copyfile(os.path.join(self.PRODUCTIONSERVER_DIR, 'nginx', 'conf', 'nginx.conf.DJANGO_BASE'), os.path.join(settings.BASE_DIR, 'nginx.conf'))
+        self.replace_text_in_file(os.path.join(settings.BASE_DIR, 'nginx.conf'), [('%%APP_PORT%%', str(self.options['port'] + 1)), ('%%SERVER_PORT%%', str(self.options['port'])), ('%%STATIC_FILE_DIR%%', settings.STATIC_ROOT), ('%%BASE_DIR%%', settings.BASE_DIR)])
+
+
+
         cherrypy.engine.start()
         cherrypy.engine.block()
+
+    @staticmethod
+    def replace_text_in_file(file_path, replacements=None):
+        if not replacements:
+            replacements = [(), ]
+
+        fh, abs_path = mkstemp()
+
+        new_file = open(abs_path, 'w')
+        old_file = open(file_path, 'r')
+
+        for line in old_file:
+            new_line = line
+            for replacement in replacements:
+                new_line = new_line.replace(replacement[0], replacement[1])
+            new_file.write(new_line)
+
+        new_file.close()
+        os.close(fh)
+        old_file.close()
+
+        os.remove(file_path)
+        shutil.move(abs_path, file_path)
